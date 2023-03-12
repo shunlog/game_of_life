fa53c6309ce83fb728890a9940533938fd67ed60
shunlog <artiombalan331@gmail.com>
2023-03-12 16:59:57 +0200

    fix resizable zone min clamp

--- godot_project/ResizableGridZone.gd
+++ godot_project/ResizableGridZone.gd
@@ -11,7 +11,6 @@ func _gui_input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
 		_pressed = event.pressed
 		if event.pressed:
-			_within_resize = $ResizeCorner._has_point(event.position)
 			_previousPosition = get_global_mouse_position()
 	if event is InputEventMouseMotion and _pressed:
 		var gl = get_global_mouse_position()
@@ -21,11 +20,14 @@ func _gui_input(event):
 func drag(from:Vector2, to:Vector2):
 	if _within_resize:
 		_wanted_size += to - from
+		_wanted_size = min_clamp(_wanted_size, Vector2(grid_size, grid_size))
 		rect_size = snap_to_grid(_wanted_size)
-		rect_size = vclamp(rect_size, Vector2(grid_size, grid_size), Vector2(INF, INF))
 	else:
 		_wanted_position += to - from
 		rect_position = snap_to_grid(_wanted_position)
+
+func min_clamp(v, vmin):
+	return vclamp(v, vmin, Vector2(INF, INF))
 
 func vclamp(v, vmin, vmax):
 	v.x = min(max(v.x, vmin.x), vmax.x)
@@ -36,3 +38,9 @@ func snap_to_grid(p:Vector2) -> Vector2:
 	p.x = int(p.x - int(p.x) % grid_size)
 	p.y = int(p.y - int(p.y) % grid_size)
 	return p
+
+func _on_ResizeCorner_mouse_entered():
+	_within_resize = true
+
+func _on_ResizeCorner_mouse_exited():
+	_within_resize = false
--- godot_project/ResizeCorner.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-
-func _ready():
-	pass
-
-func _has_point(point:Vector2):
-	return Rect2(rect_position, rect_position+rect_size).has_point(point)
