799c9bd1a104767eaa488c11f5b7128ac95a18b1
shunlog <artiombalan331@gmail.com>
2023-03-18 13:55:14 +0200

    refactor ZoneRules menu script

--- godot_project/ZoneRules.gd
+++ godot_project/ZoneRules.gd
@@ -1,10 +1,19 @@
 extends PanelContainer
 
 signal rules_changed(rules)
-export var zone := 0
 
-var rules := {Global.Rules.survival: [],
-			  Global.Rules.birth: []}
+export var zone := 0
+export var rules := {
+	Global.Rules.survival: [false, false, true, true,
+				false, false, false, false, false],
+	Global.Rules.birth: [false, false, false, true,
+			 false, false, false, false, false],
+}
+
+func _ready():
+	_create_checkboxes($HBoxContainer/Survival, Global.Rules.survival, rules[Global.Rules.survival])
+	_create_checkboxes($HBoxContainer/Birth, Global.Rules.birth, rules[Global.Rules.birth])
+	emit_signal("rules_changed", self.zone, rules)
 
 func _create_checkboxes(node, rule, values):
 	for i in range(len(values)):
@@ -13,21 +22,6 @@ func _create_checkboxes(node, rule, values):
 		node.add_child(cb)
 		cb.pressed = values[i]
 		cb.connect("toggled", self, "_on_cb_toggled", [rule, i])
-
-func _ready():
-	var arr8 := []
-	arr8.resize(9)
-	arr8.fill(false)
-	rules[Global.Rules.survival] = arr8.duplicate()
-	rules[Global.Rules.birth] = arr8.duplicate()
-	# set defaults
-	rules[Global.Rules.survival][2] = true
-	rules[Global.Rules.survival][3] = true
-	rules[Global.Rules.birth][3] = true
-	
-	_create_checkboxes($HBoxContainer/Survival, Global.Rules.survival, rules[Global.Rules.survival])
-	_create_checkboxes($HBoxContainer/Birth, Global.Rules.birth, rules[Global.Rules.birth])
-
 
 func _on_cb_toggled(pressed, rule, id):
 	rules[rule][id] = pressed
