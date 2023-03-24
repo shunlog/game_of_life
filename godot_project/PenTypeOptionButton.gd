3879c5e3c8644bf055c8d25195487c5ec9ca1610
shunlog <artiombalan331@gmail.com>
2023-03-24 19:01:19 +0200

    add shortcuts for pen type, fix others

--- godot_project/PenTypeOptionButton.gd
+++ godot_project/PenTypeOptionButton.gd
@@ -7,3 +7,15 @@ func _ready():
 
 func _on_PenTypeOptionButton_item_selected(index):
 	emit_signal("pen_type_changed", get_item_id(index))
+
+func _input(event):
+	if event.is_action_pressed("pen_type_alive"):
+		_set_selected(0)
+	elif event.is_action_pressed("pen_type_dead"):
+		_set_selected(1)
+	elif event.is_action_pressed("pen_type_infected"):
+		_set_selected(2)
+
+func _set_selected(v):
+	selected = v
+	emit_signal("pen_type_changed", get_item_id(v))
