b4390cc4d03373de7dd0002e7c00230730b3739c
shunlog <artiombalan331@gmail.com>
2023-03-31 19:20:32 +0300

    make get_files() global

--- godot_project/Game.gd
+++ godot_project/Game.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-onready var GOL_scenes = get_scenes_in_dir("res://GOL_scenes/")
+onready var GOL_scenes = Global.get_files_in_dir("res://GOL_scenes/", "tscn")
 var GOL :GameOfLife = null
 
 func _ready():
@@ -24,22 +24,6 @@ func set_GOL_scene(idx: int):
 	add_child(GOL)
 	$HUD/Control/VBoxContainer/MarginContainer/GOLControl.GOL = GOL
 	_center_camera()
-
-func get_scenes_in_dir(path) -> Array:
-	var dir = Directory.new()
-	if dir.open(path) != OK:
-		print("An error occurred when trying to access the path.")
-		return []
-	
-	var ls := []
-	dir.list_dir_begin()
-	while true:
-		var fn = dir.get_next()
-		if fn == "":
-			break
-		if !dir.current_is_dir() and fn.split(".")[-1] == "tscn":
-			ls.append(path + fn)
-	return ls
 
 
 func _on_OptionButton_item_selected(index):
--- godot_project/Global.gd
+++ godot_project/Global.gd
@@ -1,3 +1,20 @@
 extends Node
 
 enum Rules {survival, birth}
+
+
+func get_files_in_dir(path: String, ext: String) -> Array:
+	var dir = Directory.new()
+	if dir.open(path) != OK:
+		print("An error occurred when trying to access the path.")
+		return []
+	
+	var ls := []
+	dir.list_dir_begin()
+	while true:
+		var fn = dir.get_next()
+		if fn == "":
+			break
+		if !dir.current_is_dir() and fn.split(".")[-1] == ext:
+			ls.append(path + fn)
+	return ls
