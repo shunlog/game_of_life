b03f4ac2ae7c76c2d7d44811ab52771602c1f1a8
shunlog <artiombalan331@gmail.com>
2023-04-01 19:00:55 +0300

    prettier grid

--- godot_project/Grid.gd
+++ godot_project/Grid.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 var zoom := 1.0 setget _set_zoom
-export var grid_color := Color.gray
 
 func _ready():
 	update()
@@ -12,9 +11,13 @@ func _set_zoom(v):
 
 func _draw():
 	var sz = get_parent().rect_size
-	var step := 5
-	var s := max(int(zoom * 100 / step) * step, 1)
-	for x in range(sz.y / s + 1):
-		draw_line(Vector2(0, x * s), Vector2(sz.x, x * s), grid_color, 1.0)
-	for y in range(sz.x / s + 1):
-		draw_line(Vector2(y * s, 0), Vector2(y * s, sz.y), grid_color, 1.0)
+	if zoom  < .2:
+		for x in range(sz.y):
+			draw_line(Vector2(0, x), Vector2(sz.x, x), Color("#555"), 1.0)
+		for y in range(sz.x):
+			draw_line(Vector2(y, 0), Vector2(y, sz.y), Color("#555"), 1.0)
+	if zoom  < .5:
+		for x in range(0, sz.y, 20):
+			draw_line(Vector2(0, x), Vector2(sz.x, x), Color("#eee"), 1.0)
+		for y in range(0, sz.x, 20):
+			draw_line(Vector2(y, 0), Vector2(y, sz.y), Color("#eee"), 1.0)
